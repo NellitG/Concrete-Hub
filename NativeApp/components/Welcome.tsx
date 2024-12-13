1e617ff303d35e7992553044e3c5577e29288d39
NellitG <kamaunelson157@gmail.com>
2024-12-13 18:37:03 +0300

    message

--- NativeApp/components/Welcome.tsx
+++ NativeApp/components/Welcome.tsx
@@ -5,7 +5,7 @@ import { View, Text, StyleSheet } from 'react-native';
 export default function WelcomeScreen() {
   return (
     <View style={{ flex: 1 }}>
-      <Text style={styles.welcomeTxt}>Welcome to Little Lemon</Text>
+      <Text style={styles.welcomeTxt}>Welcome to Little </Text>
       <Text style={styles.welcomeDesc}>
         Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails
         in a lively but casual environment. We would love to hear your experience with us!
